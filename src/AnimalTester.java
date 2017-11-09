fd6744f05292fa0d50846a1272192d07cccacad0
yyin@seas.upenn.edu <yueyin@huntsman-ve507-0622.apn.wlan.upenn.edu>
2017-11-09 16:18:06 -0500

    add cat yue

--- src/AnimalTester.java
+++ src/AnimalTester.java
@@ -5,8 +5,10 @@ public class AnimalTester {
 		// TODO create a lion object, e.g., Animal lion = new Animal(...);
 		Animal cat_mittens = new Animal("Mittens", 4, 73, 2, "China");
 		// TODO create an owner for the lion e.g., Owner owner = new Owner(...);
+		Animal yue = new Animal("miao", 10,100, 50,"USA");
 		
 		// TODO print the name of the owner and the lion to the console
-		System.out.println(cat_mittens.getName());
+		System.out.println(cat_mittens.getName() + cat_mittens.getName());
+
 	}
 }
