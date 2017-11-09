c9f175e556e4a06cb042cb07cfe620e70358fcab
bheath015 <31784768+bheath015@users.noreply.github.com>
2017-11-09 16:11:57 -0500

    all source update

--- src/AnimalTester.java
+++ src/AnimalTester.java
@@ -3,10 +3,10 @@ public class AnimalTester {
 
 	public static void main(String[] args) {
 		// TODO create a lion object, e.g., Animal lion = new Animal(...);
-		
+		Animal cat_mittens = new Animal("Mittens", 4, 73, 2, "China");
 		// TODO create an owner for the lion e.g., Owner owner = new Owner(...);
 		
 		// TODO print the name of the owner and the lion to the console
-		
+		System.out.println(cat_mittens.getName());
 	}
 }
